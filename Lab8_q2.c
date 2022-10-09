7f7554c6833a8288a26c4d1ec548f4b88fa6bbf9
Kheerinthon Temprada <100474899+aomsinrock@users.noreply.github.com>
2022-10-09 10:44:34 +0700

    Add files via upload

---
+++ Lab8_q2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main()
+{
+    int cnt = 0;
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+        cnt++;
+        printf("child created\n");
+        while (1)
+            ;
+        printf("this line should not be show\n");
+        exit(0);
+    }
+    else
+    {
+        sleep(0);
+        if (cnt > 0)
+        {
+            kill(pid, SIGALRM);
+        }
+        printf("It die in chile %d\n", cnt+1);
+    }
+    return 0;
+}
